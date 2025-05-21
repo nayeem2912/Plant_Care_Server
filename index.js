dfdcff0707687f407771b31e8165cba6bfd58cb5
nayeem2912 <farukomar2912@gmail.com>
2025-05-21 19:38:54 +0600

    details get was added to the server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -38,6 +38,13 @@ async function run() {
       const result = await plantsCollection.find().toArray();
       res.send(result)
     })
+
+     app.get('/plants/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await plantsCollection.findOne(query);
+            res.send(result);
+        })
 
     app.post('/plants', async(req,res) =>{
       const newPlant = req.body;
